d3060933590f15770a35cbbebd3bb3e2c4950683
Ali Burak <alibarac41@icloud.com>
2023-07-12 12:29:57 +0300

    Initial Commit

---
+++ basicNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basicNoteApp
+//
+//  Created by Ali Burak Baraç on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
